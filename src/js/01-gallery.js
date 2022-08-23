ad9d4707df8b767ac583de79905f9b4223c311f1
iryna-romashchenko <irromas@ukr.net>
2022-08-23 23:05:55 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+// import { galleryItems } from './gallery-items.js';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import SimpleLightbox from 'simplelightbox';
